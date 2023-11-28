437644c8a782db28c7c44874f045e588ae41c5f7
AlbertWebs <albertmuhatia@gmail.com>
2023-11-28 17:22:11 +0300

    update

--- resources/views/admin/addColor.blade.php
+++ resources/views/admin/addColor.blade.php
@@ -18,7 +18,7 @@
         @include('admin.sidebar')
 
         <!--== BODY INNER CONTAINER ==-->
-        
+
         <div class="sb2-2">
             <div class="sb2-2-2">
                 <ul>
@@ -33,7 +33,7 @@
                     <li class="page-back"><a href="{{url('/')}}/admin/variations/color"><i class="fa fa-pencil" aria-hidden="true"></i> All Size Variations</a>
                     </li>
                 </ul>
-               
+
             </div>
             <div class="sb2-2-add-blog sb2-2-1">
                 <div class="box-inn-sp">
@@ -44,7 +44,7 @@
                             @if(Session::has('message'))
                                           <div class="alert alert-success">{{ Session::get('message') }}</div>
                            @endif
-           
+
                            @if(Session::has('messageError'))
                                           <div class="alert alert-danger">{{ Session::get('messageError') }}</div>
                            @endif
@@ -54,12 +54,9 @@
                         <form method="POST" action="{{url('/')}}/admin/add_Variation" enctype="multipart/form-data">
                             {{csrf_field()}}
                             <div class="row">
-                               
                                 {{--  --}}
                                 <div class="input-field col s12">
-
                                     <div class="row">
-                                
                                         {{--  --}}
                                         <div class="input-field col s12">
                                             <select required name="value" class="icons" id="mydiv">
@@ -73,9 +70,7 @@
                                             </select>
                                             <label>Choose Color</label>
                                         </div>
-                                    
                                         <div class="section-space col s12"></div>
-                                    
                                     </div>
                                 </div>
                                 {{--  --}}
@@ -97,7 +92,7 @@
                                 </div>
                             </div>
                             <div class="row">
-                             
+
                                 {{--  --}}
                                 <div class="input-field col s12">
                                     <select required name="product_id" class="icons" id="mydiv">
@@ -108,14 +103,14 @@
                                     </select>
                                     <label>Choose Product</label>
                                 </div>
-                             
+
                                 <div class="section-space col s12"></div>
-                               
+
                             </div>
 
-                            
-                           
-                   
+
+
+
                             <div class="row">
                                 <div class="input-field col s12">
                                     <input  type="submit" class="waves-effect waves-light btn-large" value="Submit">
@@ -154,13 +149,13 @@
                                     <input  type="submit" class="waves-effect waves-light btn-large" value="Submit">
                                 </div>
                             </div>
-                            
+
                             <div class="tab-inn" id="loading-bar">
                                 <div class="progress">
                                     <div class="indeterminate"></div>
                                 </div>
                             </div>
-                            
+
                         </form>
                     </div>
                 </div>
@@ -175,10 +170,10 @@
     $('#categoryAddForm').on('submit',function(event){
         event.preventDefault();
         $('#loading-bar').show();
-   
+
 
         let title = $('#CategoryTitle').val();
-       
+
 
         $.ajax({
           url: "{{url('/')}}/admin/addCategoryAjaxRequest",
